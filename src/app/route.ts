6faa77f7d28a68c49eeafc690cbc4423a1342bd6
dev-khs <kyo3553@naver.com>
2024-08-23 19:55:57 +0900

    feat: implement router functions

--- src/app/route.ts
+++ src/app/route.ts
@@ -12,3 +12,26 @@ const routes: Routes[] = [
     view: ArticlePage,
   },
 ]
+
+// Find the route that matches the current path.
+const findRoute = (path: string) => {
+  return routes.find(route => route.path === path)
+}
+
+// Render route.
+const renderRoute = () => {
+  const route = findRoute(window.location.pathname)
+
+  if (route) {
+    route.view()
+  }
+}
+
+// Navigation function.
+export const navigateTo = (path: string) => {
+  window.history.pushState(null, '', path)
+}
+
+renderRoute()
+
+window.addEventListener('popstate', renderRoute)
