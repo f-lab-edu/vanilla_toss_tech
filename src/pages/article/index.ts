fc88ec49abeaba1f184798a3745355d8df818f2a
dev-khs <kyo3553@naver.com>
2024-08-23 19:59:06 +0900

    feat: apply navigation feature to the buttons

--- src/pages/article/index.ts
+++ src/pages/article/index.ts
@@ -1,3 +1,4 @@
+import {navigateTo} from 'src/app/route'
 import './index.css'
 
 export const ArticlePage = () => {
@@ -7,8 +8,16 @@ export const ArticlePage = () => {
     appElement.innerHTML = `
       <div>
         <h1>Hello, I'm Article!</h1>
+        <button id="navigation">home으로 돌아가기</button>
       </div>
     `
+
+    const button = document.getElementById('navigation')
+    if (button) {
+      button.addEventListener('click', () => {
+        navigateTo('/')
+      })
+    }
   } else {
     document.body.innerHTML = `
       <h1>Default Content</h1>
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,3 +1,4 @@
+import {navigateTo} from 'src/app/route'
 import './index.css'
 
 export const HomePage = () => {
@@ -7,8 +8,16 @@ export const HomePage = () => {
     appElement.innerHTML = `
       <div>
         <h1>Hello, World!</h1>
+        <button id="navigation">article로 이동하기</button>
       </div>
     `
+
+    const button = document.getElementById('navigation')
+    if (button) {
+      button.addEventListener('click', () => {
+        navigateTo('/article')
+      })
+    }
   } else {
     document.body.innerHTML = `
       <h1>Default Content</h1>
